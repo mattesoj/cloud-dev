870111ff049a29b4887e3fa541fa2a45a8bd2806
Josh Matteson <jmatteson@proworks.com>
2018-10-02 13:15:12 -0700

    Custom code

---
+++ MyCloudSite.Core/Controllers/GithubHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Octokit;
+using ProductHeaderValue = Octokit.ProductHeaderValue;
+
+namespace MyCloudSite.Core.Controllers
+{
+	public class GithubHelper
+	{
+		public static IReadOnlyList<Repository> GetRepositories(string organisation)
+		{
+			var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+			return client.Repository.GetAllForOrg(organisation).Result;
+		}
+	}
+}
